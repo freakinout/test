d7729c47e67ef6d21486d9c2455a9db26852cfcf
Sascha Berger <sascha.elvis@gmail.com>
2019-11-19 19:57:20 +0100

    First Training on third try

--- tf_thirdTry.py
+++ tf_thirdTry.py
@@ -11,24 +11,26 @@ from pprint import pprint
 IMG_WIDTH=30
 IMG_HEIGHT=30
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE=50
+BATCH_SIZE=50000
 
 
 script_path = os.path.dirname(os.path.realpath(__file__))+"\\"
 os.chdir(script_path)
-img_path = script_path+"letterimgs\\"
+img_path = script_path+"letterimgs_50k_arial\\"
 
 
 f = open(img_path+"labels.txt")
 labels = f.readlines()
 f.close()
 
-for i in range(len(labels)): labels[i] = (labels[i].split(";")[0])
 
-h = []
-for i in range(ord("a"),ord("z")+1): h.append(chr(i))
-h = np.asarray(h)
 
+"""for i in range(len(labels)): 
+  q = labels[i].split(";")
+  labels[i] = [ord(q[0]),q[1]]
+"""
+labels = np.asarray(labels)
+labels = tf.data.Dataset.from_tensor_slices(labels)
 
 def decode_img(img):
   # convert the compressed string to a 3D uint8 tensor
@@ -53,14 +55,14 @@ def process_path(file_path):
   return img, label
 
 
-list_ds = tf.data.Dataset.list_files(tf.strings.join([img_path,'*.bmp']))
-
-labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-""" for image, label in labeled_ds.take(3):
-  print("Image shape: ", image.numpy().shape)
-  print("Label: ", label.numpy()) """
-
+def process_labels(label_arr):
+  helper = tf.strings.split(label_arr,sep=";")
+  l = tf.math.subtract(tf.strings.unicode_decode(tf.gather(helper,0),input_encoding='UTF-8'),tf.strings.unicode_decode("a",input_encoding='UTF-8'))
+  img = tf.strings.join([img_path,tf.strings.strip(tf.gather(helper,1)),".bmp"])
+  img = tf.io.read_file(img)
+  img = tf.image.decode_bmp(img, channels=0)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT]) , l
 
 def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
   # This is a small dataset, only load it once, and keep it in memory.
@@ -85,11 +87,21 @@ def prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
 
   return ds
 
-train_ds = prepare_for_training(labeled_ds)
+
+list_ds = labels.map(process_labels, num_parallel_calls=AUTOTUNE)
+
+"""
+for image, label in list_ds.take(3):
+  print("Image shape: ", image.numpy().shape)
+  print("Label: ", label.numpy()) 
+
+"""
+
+train_ds = prepare_for_training(list_ds)
 
 image_batch, label_batch = next(iter(train_ds))
 
-#print("####")
+print("####")
 print(label_batch.shape)
 
 inputs = keras.Input(shape=(30,30,1), name='digits')
@@ -120,9 +132,11 @@ print('# Fit model on training data')
 history = model.fit(image_batch,
                     label_batch,
                     #batch_size=50,
-                    epochs=3
+                    epochs=10
                     # We pass some validation for
                     # monitoring validation loss and metrics
                     # at the end of each epoch
                     #validation_data=(x_val, y_val)
                     )
+
+model.save(img_path+"myModel.model")
